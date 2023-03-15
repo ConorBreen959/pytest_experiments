0f8fe3874916bba67286113ca2567e8fbac1517c
Conor Breen <conor.breen@plusvital.com>
2023-03-15 15:33:46 +0000

    Initial commit

---
+++ test/test_app.py
@@ -0,0 +1,9 @@
+from pytest_experiments.bin.calculate_age import get_age
+
+def test_get_age():
+    # Given.
+    yyyy, mm, dd = map(int, "1996/07/11".split("/"))
+    # When.
+    age = get_age(yyyy, mm, dd)
+    # Then.
+    assert age == 27
